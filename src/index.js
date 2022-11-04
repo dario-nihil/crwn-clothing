9717d8dd550644b8c99223f637cffc00896e2e48
dario-nihil <dario.nihil@gmail.com>
2022-11-04 12:39:50 +0100

    added shop component and ceated mock data t0 start working with mock data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { UserProvider } from "./context/user-context";
+import { UserProvider } from "./context/user.context";
+import { ProductProvider } from "./context/products.context";
 
 import "./index.scss";
 
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
