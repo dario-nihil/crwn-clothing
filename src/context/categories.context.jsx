b941fdfe0f0f64394544430ca390f47b2c13f4d2
dario-nihil <dario.nihil@gmail.com>
2022-11-08 12:17:32 +0100

    renamed products context into categories context and fetched data from firestore

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -2,26 +2,26 @@ import { createContext, useState, useEffect } from "react";
 
 import { getCategoriesAndDocument } from "../utils/firebase/firebase.utils";
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductProvider = ({ children }) => {
-  const [products, serPorducts] = useState([]);
-  const value = { products };
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  const value = { categoriesMap };
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocument();
-      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
 
     getCategoriesMap();
   }, []);
 
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
